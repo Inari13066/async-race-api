09c6bf804350d56abe41f44e6a6defc20a2ca66d
Inari13066 <devil13066@gmail.com>
2023-07-12 03:59:26 +0400

    feat: add works, start layout

--- src/index.js
+++ src/index.js
@@ -1,128 +1,29 @@
-import jsonServer from "json-server";
+import "./styles.css";
 
-const db = {
-    garage: [
-        {
-            name: "Tesla",
-            color: "#e6e6fa",
-            id: 1,
-        },
-        {
-            name: "BMW",
-            color: "#fede00",
-            id: 2,
-        },
-        {
-            name: "Mersedes",
-            color: "#6c779f",
-            id: 3,
-        },
-        {
-            name: "Ford",
-            color: "#ef3c40",
-            id: 4,
-        },
-    ],
-    winners: [
-        {
-            id: 1,
-            wins: 1,
-            time: 10,
-        },
-    ],
-};
+const body = document.querySelector("body");
+const createCarForm = document.createElement("form");
+createCarForm.classList.add("create-update-form");
+const updateCarForm = document.createElement("form");
+updateCarForm.classList.add("create-update-form");
+const nameInput = document.createElement("input");
+nameInput.className = "form-control";
 
-const server = jsonServer.create();
-const router = jsonServer.router(db);
-const middlewares = jsonServer.defaults();
+const colorInput = document.createElement("input");
+colorInput.type = "color";
+colorInput.classList.add("form-control", "form-control-color");
 
-const body = document.querySelector(body);
-const testText = document.createElement("p");
-testText.innerHTML = db.garage[0].name;
-const testImg = document.createElement("img");
-testImg.src = "./assets/img/car.png";
-testImg.alt = "car";
-body.append(testImg);
-body.append(testText);
+const nameInputUPD = nameInput.cloneNode(true);
+const colorInputUPD = colorInput.cloneNode(true);
 
-const PORT = 8080;
+nameInput.id = "create-car-name";
+colorInput.id = "create-car-color";
+createCarForm.append(nameInput);
+createCarForm.append(colorInput);
+body.append(createCarForm);
 
-const state = { velocity: {}, blocked: {} };
 
-server.use(middlewares);
-
-server.patch("/engine", (req, res) => {
-    const { id, status } = req.query;
-
-    if (!id || !status || !/^(started)|(stopped)|(drive)$/.test(status)) {
-        return res
-            .status(400)
-            .send(
-                'Wrong parameters: "id" should be any positive number, "status" should be "started", "stopped" or "drive"'
-            );
-    }
-
-    if (!db.garage.find((car) => car.id === +id)) {
-        return res.status(404).send("Car with such id was not found in the garage.");
-    }
-
-    const distance = 500000;
-    if (status === "drive") {
-        const velocity = state.velocity[id];
-
-        if (!velocity)
-            return res
-                .status(404)
-                .send(
-                    'Engine parameters for car with such id was not found in the garage. Have you tried to set engine status to "started" before?'
-                );
-        if (state.blocked[id])
-            return res
-                .status(429)
-                .send("Drive already in progress. You can't run drive for the same car twice while it's not stopped.");
-
-        state.blocked[id] = true;
-
-        const x = Math.round(distance / velocity);
-
-        if (new Date().getMilliseconds() % 3 === 0) {
-            setTimeout(() => {
-                delete state.velocity[id];
-                delete state.blocked[id];
-                res.header("Content-Type", "application/json")
-                    .status(500)
-                    .send("Car has been stopped suddenly. It's engine was broken down.");
-            }, (Math.random() * x) ^ 0);
-        } else {
-            setTimeout(() => {
-                delete state.velocity[id];
-                delete state.blocked[id];
-                res.header("Content-Type", "application/json")
-                    .status(200)
-                    .send(JSON.stringify({ success: true }));
-            }, x);
-        }
-    } else {
-        const x = req.query.speed ? +req.query.speed : (Math.random() * 2000) ^ 0;
-
-        const velocity = status === "started" ? Math.max(50, (Math.random() * 200) ^ 0) : 0;
-
-        if (velocity) {
-            state.velocity[id] = velocity;
-        } else {
-            delete state.velocity[id];
-            delete state.blocked[id];
-        }
-
-        setTimeout(
-            () =>
-                res.header("Content-Type", "application/json").status(200).send(JSON.stringify({ velocity, distance })),
-            x
-        );
-    }
-});
-
-server.use(router);
-server.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
-});
+nameInputUPD.id = "update-car-name";
+colorInputUPD.id = "update-car-color";
+updateCarForm.append(nameInputUPD);
+updateCarForm.append(colorInputUPD);
+body.append(updateCarForm);
