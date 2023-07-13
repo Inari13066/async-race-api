b66b7caa13a3d10a0888f4cf8d74ec02580473f3
Inari13066 <devil13066@gmail.com>
2023-07-13 12:23:11 +0400

    feat: create first page main layout

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const createCarForm = document.createElement("form");
 createCarForm.classList.add("create-update-form");
 const updateCarForm = document.createElement("form");
 updateCarForm.classList.add("create-update-form");
+
 const nameInput = document.createElement("input");
 nameInput.className = "form-control";
 
@@ -12,18 +13,51 @@ const colorInput = document.createElement("input");
 colorInput.type = "color";
 colorInput.classList.add("form-control", "form-control-color");
 
+const createCarBtn = document.createElement("input");
+createCarBtn.classList.add("btn", "btn-warning");
+createCarBtn.type = "submit";
+const updateCarBtn = createCarBtn.cloneNode(true);
+createCarBtn.value = "Create";
+updateCarBtn.value = "Update";
+
 const nameInputUPD = nameInput.cloneNode(true);
 const colorInputUPD = colorInput.cloneNode(true);
 
 nameInput.id = "create-car-name";
 colorInput.id = "create-car-color";
-createCarForm.append(nameInput);
-createCarForm.append(colorInput);
-body.append(createCarForm);
-
+createCarForm.append(nameInput, colorInput, createCarBtn);
 
 nameInputUPD.id = "update-car-name";
 colorInputUPD.id = "update-car-color";
-updateCarForm.append(nameInputUPD);
-updateCarForm.append(colorInputUPD);
-body.append(updateCarForm);
+updateCarForm.append(nameInputUPD, colorInputUPD, updateCarBtn);
+
+body.append(createCarForm, updateCarForm);
+
+const mainBtnContainer = document.createElement("div");
+mainBtnContainer.classList.add("main-btn-container");
+
+const raceBtn = document.createElement("button");
+raceBtn.classList.add("btn", "btn-info");
+const resetBtn = raceBtn.cloneNode(true);
+raceBtn.innerHTML = "Race";
+resetBtn.innerHTML = "Reset";
+const generateBtn = document.createElement("button");
+generateBtn.classList.add("btn", "btn-warning");
+generateBtn.innerHTML = "Generate cars";
+
+mainBtnContainer.append(raceBtn, resetBtn, generateBtn);
+body.append(mainBtnContainer);
+
+const mainContainer = document.createElement("div");
+mainContainer.classList.add("main-container")
+
+const garageTitle = document.createElement("h1");
+let carsCount = 104; //TODO!!! From server//
+garageTitle.innerHTML = `Garage (${carsCount})`;
+
+const pageTitle = document.createElement("h2");
+let pageN = 1; //TODO!!!//
+pageTitle.innerHTML = `Page #${pageN}`;
+
+mainContainer.append(garageTitle, pageTitle);
+body.append(mainContainer);
