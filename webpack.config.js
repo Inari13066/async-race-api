e6a9cf30ff233ff97a3c6250236420b532b7f794
Inari13066 <devil13066@gmail.com>
2023-07-13 18:03:18 +0400

    feat: finish main layout blocks for garage view

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,44 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const baseConfig = {
-    target: 'web',
-    entry: path.resolve(__dirname, './src/index.js'),
-    mode: 'development',
+    target: "web",
+    entry: path.resolve(__dirname, "./src/index.js"),
+    mode: "development",
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ["style-loader", "css-loader"],
             },
-            { test: /\.([cm]?ts|tsx)$/, exclude: /node_modules/, use: { loader: 'ts-loader' } },
+            { test: /\.([cm]?ts|tsx)$/, exclude: /node_modules/, use: { loader: "ts-loader" } },
+            {
+                test: /\.svg$/,
+                loader: "svg-inline-loader",
+            },
         ],
     },
     resolve: {
-        extensions: ['.js', '.css', '.ts'],
+        extensions: [".js", ".css", ".ts"],
     },
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, './public'),
+        filename: "index.js",
+        path: path.resolve(__dirname, "./public"),
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
+            template: path.resolve(__dirname, "./src/index.html"),
+            filename: "index.html",
         }),
         new CleanWebpackPlugin(),
     ],
 };
 
 module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+    const isProductionMode = mode === "prod";
+    const envConfig = isProductionMode ? require("./webpack.prod.config") : require("./webpack.dev.config");
 
     return merge(baseConfig, envConfig);
 };
